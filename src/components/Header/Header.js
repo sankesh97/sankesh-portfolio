68171fbe10726e2d6768e2fabc693cb8255a83ce
sankesh97 <ramojitinorange@gmail.com>
2025-02-11 14:58:17 +0530

    Added Font and Header Changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,15 @@ import Image from 'next/image';
 import Link from 'next/link';
 import classes from '@/components/Header/Header.module.css';
 
+
 const Header = () => {
     return <div className='h-[120px] flex justify-between items-center px-10'>
         <Image className='max-w-[330px]' src="/assets/images/logo.svg" alt="logo" width={330} height={65} />
-        <div className={`flex gap-32`}>
-            <Link href="/about">About</Link>
-            <Link href="/work">Work</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/contact">Contact</Link>
+        <div className={`flex gap-32 font-karla`}>
+            <Link className={`p-4`} href="/about">About</Link>
+            <Link className={`p-4`} href="/work">Work</Link>
+            <Link className={`p-4`} href="/blog">Blog</Link>
+            <Link className={`${classes.ctabutton} p-4`} href="/contact">Contact</Link>
         </div>
     </div>
 }
